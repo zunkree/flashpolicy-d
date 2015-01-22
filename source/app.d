facf503d6a0a5989c9e51ab5a3227e1955c05c70
zunkree aka Aleksandr Milushev <zunkree@ciklum.com>
2015-01-22 15:59:13 +0200

    Allow bind to list of ports or addresses

--- source/app.d
+++ source/app.d
@@ -1,28 +1,29 @@
 import vibe.d;
+import std.array;
 
 
-enum DEFAULT_PORT = 843;
-enum DEFAULT_BIND = "127.0.0.1";
+enum DEFAULT_PORT =  843;
+enum DEFAULT_BIND = "0.0.0.0";
 enum DEFAULT_FILE = "public/flashpolicy.xml";
 
 enum POLICY_FILE_REQ = cast(ubyte[])"<policy-file-request/>\0";
 
 
 struct policyOpts {
-	ushort port;
-	string bind;
-	string file;
+	string file = DEFAULT_FILE;
+	string[] binds = [DEFAULT_BIND];
+	ushort[] ports = [DEFAULT_PORT];
 }
 
 policyOpts getPolicyOpts ()
 {
 	policyOpts opts;
-	if (!getOption("port|p", &opts.port, format("bind on port, default %d", DEFAULT_PORT)))
-		opts.port = DEFAULT_PORT;
-	if (!getOption("bind|b", &opts.bind, format("bind on addr, default \"%s\"", DEFAULT_BIND)))
-		opts.bind = DEFAULT_BIND;
-	if (!getOption("file|f", &opts.file, format("path to policy.xml, defautl \"%s\"", DEFAULT_FILE)))
-		opts.file = DEFAULT_FILE;
+	string ports, binds;
+	getOption("file|f", &opts.file, format("path to policy.xml, defautl \"%s\"", DEFAULT_FILE));
+	getOption("bind|b", &binds, format("bind on comma separated list of addrs, default %s", DEFAULT_BIND));
+	getOption("port|p", &ports, format("bind on comma separated list of ports, default %d", DEFAULT_PORT));
+	if (binds) opts.binds = binds.split(",");
+	if (ports) opts.ports = ports.split(",").map!(a=>to!ushort(a)).array;
 	return opts;
 }
 
@@ -30,9 +31,8 @@ void handleFlashPolicy(TCPConnection stream, string policy)
 {
 	ubyte[POLICY_FILE_REQ.length] buf;
 	stream.read(buf);
-	if (POLICY_FILE_REQ == buf) {
+	if (POLICY_FILE_REQ == buf)
 		stream.write(policy);
-	}
 	stream.close;
 }
 
@@ -40,5 +40,7 @@ shared static this()
 {
 	auto opts = getPolicyOpts();
 	string policy = readFileUTF8(opts.file);
-	listenTCP(opts.port, stream => handleFlashPolicy(stream, policy), opts.bind);
+	foreach (bind; opts.binds)
+		foreach (port; opts.ports)
+			listenTCP(port, stream => handleFlashPolicy(stream, policy), bind);
 }
